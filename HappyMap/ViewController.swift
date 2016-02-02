59ba9af02389f1a053d6d919c25036da8c57fcb9
Igor Morenko <morenko@lionsoft.ru>
2016-02-02 22:21:45 +0300

    Initial Commit

---
+++ HappyMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HappyMap
+//
+//  Created by Игорь Моренко on 02.02.16.
+//  Copyright © 2016 LionSoft LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
